faca7b045f8cac14ffbe1bf44af65dd077e06e30
JOYSU <JOYSU@DESKTOP-7BCLD47>
2022-05-21 17:45:11 +0900

    status-subin

---
+++ GitTest/src/status.java
@@ -0,0 +1,20 @@
+
+public class status {
+
+
+	public void status(String a){
+		System.out.println("NAME : " + p_name +"LV : " + p_lv +"HP : "+ p_hp + 
+				"EXP : " + p_exp + "현재 상태 : " + pata_condition + "누적 일수 : " + p_time );
+		System.out.println("현재 경험치를 확인하시겠습니까? ");
+		
+		
+	}
+
+	public void exp(String b) {
+		System.out.println("현재 경험치 : "+ p_exp);
+	}
+	
+	public void exit(String c) {
+		
+	}
+}
