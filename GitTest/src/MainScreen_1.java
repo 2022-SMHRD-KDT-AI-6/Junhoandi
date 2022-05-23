560bb89a4d4dbf7bb8e2870f6c49e87a5cb756e4
smhrd <smhrd@DESKTOP-LUL7B9D>
2022-05-23 09:53:01 +0900

    메인화면 선택지 및 로그인 성공시 출력할 텍스트 추가 - 명호

---
+++ GitTest/src/MainScreen_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainScreen_1 {
+	
+	public void logIn(){
+		  Scanner sc = new Scanner(System.in);
+	        System.out.println("[1] 회원가입 || [2]로그인 || [3] 종료");
+	        int ans = sc.nextInt();
+	        
+	        
+	        sc.close();
+	        
+	}
+
+	
+	
+} 
