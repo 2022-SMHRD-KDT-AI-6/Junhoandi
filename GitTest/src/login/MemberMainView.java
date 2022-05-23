9544742d8e598292a589cc1a140b05e19c16dc7e
smhrd <smhrd@DESKTOP-LUL7B9D>
2022-05-23 10:18:29 +0900

    Merge branch 'master' of https://github.com/2022-SMHRD-KDT-AI-6/Patamon.git

--- GitTest/src/login/MemberMainView.java
+++ GitTest/src/login/MemberMainView.java
@@ -4,55 +4,76 @@ import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class MemberMainView {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		Scanner sc = new Scanner(System.in);
 		MemberController controller = new MemberController();
 		ResultSet rs;
-		System.out.println("[1]ȸ������ [2]�α��� [3]���� ");
+		System.out.println("[1]회원가입 [2]로그인 [3]종료 ");
 		int row = 0;
 		while (true) {
 			int menu = sc.nextInt();
 			if (menu == 1) {
-				System.out.println("==ȸ�� ����==");
-				System.out.println("ID�� �Է����ּ��� >> ");
+				System.out.println("==회원 가입==");
+				System.out.println("ID를 입력해주세요 >> ");
 				String id = sc.next();
-				System.out.println("pw�� �Է����ּ��� >> ");
+				System.out.println("pw를 입력해주세요 >> ");
 				String pw = sc.next();
-				System.out.println("�̸��� �Է����ּ��� >> ");
+				System.out.println("이름을 입력해주세요 >> ");
 				String name = sc.next();
 
-				// insert �޼ҵ带 ȣ��!
+				// insert 메소드를 호출!
 				MemberDto dto = new MemberDto(id, pw, name);
 				row = controller.insert(dto);
 				if (row > 0) {
-					System.out.println("������ �߰� ����!!!!");
+					System.out.println("데이터 추가 성공!!!!");
 				} else {
-					System.out.println("�߰� ����!!!!");
+					System.out.println("추가 실패!!!!");
 				}
 
 			} else if (menu == 2) {
 
-				System.out.println("==�α��� �ϱ�==");
-				System.out.print("�̸� �Է� >> ");
+				System.out.println("==로그인 하기==");
+				System.out.print("이름 입력 >> ");
 				String inputName = sc.next();
 				MemberDto dto = new MemberDto(inputName);
 				dto = controller.select(dto);
 				if (dto.getId() != null && dto.getPw() != null) {
-					System.out.println("�α��� ����");
-					// �α����� �����ϸ� �Ĵڸ� Ű��� �����ִ� â �ҷ�����
+					System.out.println("로그인 성공");
 					
+					String art1 = """
+							 ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+							▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+							▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ 
+							▐░▌          ▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌               ▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     
+							▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▐░▌ ▐░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     
+							▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     
+							▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▀   ▐░▌▐░█▀▀▀▀▀▀▀▀▀       ▀▀▀▀▀▀▀▀▀█░▌     ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀█░█▀▀      ▐░▌     
+							▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌                         ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌     ▐░▌       ▐░▌     
+							▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░▌       ▐░▌▐░▌      ▐░▌      ▐░▌     
+							▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌     ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     
+							 ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀         ▀  ▀         ▀       ▀      
+							                                                                                                                          
+							                                                                                  
+										
+										""";
+					
+					for (int i = 0; i < art1.length(); i++) {
+						System.out.print(art1.charAt(i));
+						Thread.sleep(2);
 				}
+				
 
 				if (dto.getId() == null || dto.getPw() == null) {
-					System.out.println("�α��� ����");
+					System.out.println("로그인 실패");
 				}
 			}
 
 			else if (menu == 3) {
-				System.out.println("���α׷��� ����˴ϴ�.");
+				System.out.println("프로그램이 종료됩니다.");
 				break;
 			}
 		}
+	}
 	}
 }
